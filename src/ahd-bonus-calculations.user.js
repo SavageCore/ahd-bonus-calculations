8516b18ba5494f31afcfc462ca51a49525ec5dff
SavageCore <talk@savagecore.eu>
2017-04-24 18:51:22 +0100

    First Commit

---
+++ src/ahd-bonus-calculations.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         AHD Bonus Calculations
+// @namespace    https://savagecore.eu
+// @version      0.1.0
+// @description  Calculate remaining points needed
+// @author       SavageCore
+// @include      http*://awesome-hd.me/bonus.php*
+// @grant        none
+// ==/UserScript==
+//
+/* global document */
+
+(function () {
+	'use strict';
+
+	const currentPoints = parseInt(document.getElementById('pointsStats').innerText.replace(/,/g, ''), 10);
+	const pointsPerDay = parseInt(document.querySelector('#content > div > h3').innerText.replace(/^\D+|[,.]/g, ''), 10);
+	const table = document.querySelector('#content > div > div > table');
+
+	for (let i = 0; i < table.rows.length; i++) {
+		const row = table.rows[i];
+		for (let j = 0; j < row.cells.length; j++) {
+			const col = row.cells[j];
+			if (i === 0 && col.innerText.indexOf('Not enough points!') !== -1) {
+				const cost = parseInt(col.innerText.match(/(.*) Points/g)[0].replace(/^\D+|,/g, ''), 10);
+				const remainingPoints = cost - currentPoints;
+				const timeLeft = remainingPoints / pointsPerDay;
+				col.title = 'Days left at current rate: ' + timeLeft;
+			}
+		}
+	}
+})();
