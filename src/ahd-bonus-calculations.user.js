b267bc3f932901d43f3c8c0f4edd6118517a825d
SavageCore <talk@savagecore.eu>
2019-10-27 22:42:26 +0000

    Fix XO

--- src/ahd-bonus-calculations.user.js
+++ src/ahd-bonus-calculations.user.js
@@ -15,23 +15,23 @@
 (function () {
 	'use strict';
 
-	const pointsPerDay = parseInt(document.querySelector('#content > div > h3').innerText.replace(/^\D+|[,.]/g, ''), 10);
+	const pointsPerDay = parseInt(document.querySelector('#content > div > h3').textContent.replace(/^\D+|[,.]/g, ''), 10);
 	if (pointsPerDay > 0) {
-		const currentPoints = parseInt(document.querySelector('#pointsStats').innerText.replace(/,/g, ''), 10);
+		const currentPoints = parseInt(document.querySelector('#pointsStats').textContent.replace(/,/g, ''), 10);
 		const table = document.querySelector('#content > div > div > table');
 
 		for (let i = 0; i < table.rows.length; i++) {
 			const row = table.rows[i];
 			for (let j = 0; j < row.cells.length; j++) {
 				const col = row.cells[j];
-				if (i === 0 && col.innerText.indexOf('Not enough points!') !== -1) {
-					const cost = parseInt(col.innerText.match(/(.*) Points/g)[0].replace(/,/g, ''), 10);
+				if (i === 0 && col.textContent.includes('Not enough points!')) {
+					const cost = parseInt(col.textContent.match(/(.*) Points/g)[0].replace(/,/g, ''), 10);
 					const remainingPoints = cost - currentPoints;
 					const timeLeft = remainingPoints / pointsPerDay;
-                    const secondsLeft = (timeLeft - Math.floor(timeLeft)) * 86400
-                    const projectedDate = new Date();
-                    projectedDate.setDate(projectedDate.getDate() + timeLeft);
-                    projectedDate.setSeconds(secondsLeft);
+					const secondsLeft = (timeLeft - Math.floor(timeLeft)) * 86400;
+					const projectedDate = new Date();
+					projectedDate.setDate(projectedDate.getDate() + timeLeft);
+					projectedDate.setSeconds(secondsLeft);
 					col.title = 'Exact days left: ' + timeLeft;
 					col.innerHTML = col.innerHTML.replace(/Not enough points!/, remainingPoints.toLocaleString() + ' Points remaining<br />' + humaneDate(projectedDate) + ' left');
 				}
